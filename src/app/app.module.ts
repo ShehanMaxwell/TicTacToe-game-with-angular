b3f9e7351667251ae9f56728a68a2ff0bb0ac74f
Shehan Maxwell <shehanbbn@gmail.com>
2023-04-12 20:44:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: ` <button>{{ value }}</button> `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
+})
+export class SquareComponent {
+  @Input()
+  value!: 'X' | 'O';
+}
